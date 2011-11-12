92caa417756b7ba7604c5bbf0c625db5f5b3af7f
Stefan <stefan.roock@it-agile.de>
2011-11-12 21:03:30 +0100

    Heroku

--- src/samplewebapp/main_heroku.clj
+++ src/samplewebapp/main_heroku.clj
@@ -1,15 +1,13 @@
 (ns samplewebapp.webcontroller
   (:use ring.adapter.jetty)
-  (:use ring.middleware.stacktrace)
   (:use samplewebapp.routes)
   (:require [compojure.handler :as handler]))
 ; Ring provides that adapter to Jetty and the underlying HTTP requests and responses.
-; Difference to main-local.clj is that automatic reloading is not available under Heroku.
+; Difference to main-local.clj is that automatic reloading and browser stacktrace is not available under Heroku.
 
 (def app-heroku
 	(-> #'main-routes ; use the routes defined in main-routes 
-	    (handler/api)  ; neccessary to access the form data in the paramter style way in the route definitions
-	    (wrap-stacktrace))) ; show stacktrace in browser when exceptions are thrown in the server
+	    (handler/api)))  ; neccessary to access the form data in the paramter style way in the route definitions
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
