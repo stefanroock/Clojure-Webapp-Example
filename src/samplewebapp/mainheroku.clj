01bd1f8c8fdecc3ebc17e71065f361fbeba97fe7
Stefan <stefan.roock@it-agile.de>
2011-11-12 21:15:55 +0100

    Heroku

--- src/samplewebapp/mainheroku.clj
+++ src/samplewebapp/mainheroku.clj
@@ -9,6 +9,11 @@
 	(-> #'main-routes ; use the routes defined in main-routes 
 	    (handler/api)))  ; neccessary to access the form data in the paramter style way in the route definitions
 
+(defn app [req]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello from Clojure!\n"})
+
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
-    (run-jetty app-heroku {:port port}))) ; run on Heroku
+    (run-jetty app {:port port}))) ; run on Heroku
