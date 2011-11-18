92ee898b1bced5b971a8d5b2e4e107dd84ac46eb
Stefan <stefan.roock@it-agile.de>
2011-11-18 08:04:45 +0100

    Fixed dev dependencies - hopefully. Added an example for get params

--- src/samplewebapp/actions.clj
+++ src/samplewebapp/actions.clj
@@ -8,11 +8,16 @@
 	    [:head [:title title]]
 	    [:body [:h1.header title] body])) 
 
+(defn welcome-page []
+	(layout "Welcome to Clojure Sample Webapp"
+	  [:p "The webapp shows how to use Clojure with Jetty, Ring, Compojure and Hiccup to build a web application."]))
+
 (defn full-name [first second]
 	(layout "Full Name" (str first " " second)))
 
 (defn name-form []
-	(layout (form-to [:post "/post-name"]
+	(layout "Name Form"
+	  (form-to [:post "/post-name"]
 	  (label :first-name "First Name") (text-field :first-name) [:br] (label :second-name "Second Name") (text-field :second-name) 
 	  [:p (submit-button "Submit")])))
 	
--- src/samplewebapp/routes.clj
+++ src/samplewebapp/routes.clj
@@ -5,8 +5,9 @@
 ; Compojure provides an easy to use DSL for route definitions (on top of Ring)
 
 (defroutes main-routes
-   (GET "/" [] "<h1>Sample Clojure Webapp</h1>")
+   (GET "/" [] (welcome-page))
    (GET "/name-form" [] (name-form)) ; simple GET request that returns a web page with a form
    (GET "/full-name/:first/:second" [first second] (full-name first second)) ; GET request with variable URL
+   (GET "/full-name" [first second] (full-name first second)) ; GET request with request params
    (POST "/post-name" [first-name second-name] (post-name first-name second-name)) ; POST request with form data
    (route/not-found "<h1>Page not found</h1>")) ; unknown URL
